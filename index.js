9dd5293848816b8ab1ee4fc9278ba4841da771e8
Katherine Gruenberg <katherine.gruenberg@gmail.com>
2018-05-05 15:59:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -49,19 +49,19 @@ class Passenger {
 
 class Trip {
   constructor(driver, passenger) {
-    this.id = ++tripID;
+    this.id = ++tripId;
     this.driverId = driver.id;
     this.passengerId = passenger.id;
 
     store.trips.push(this);
-  };
+  }
 
 
   driver() {
     return store.drivers.find(driver => {
       return driver.id === this.driverId;
-    })
-  };
+    });
+  }
 
  passenger() {
       return store.passengers.find(passenger => {
