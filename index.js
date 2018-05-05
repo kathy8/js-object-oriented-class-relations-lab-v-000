c282bcca4021c524a705d6d2211e91f8c404b91c
Katherine Gruenberg <katherine.gruenberg@gmail.com>
2018-05-05 15:09:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ let tripId = 0;
     store.drivers.push(this);
   };
 
- trips() {
- return store.trips.filter(trip => {
+ trips(){
+ return store.trips.filter(trip=>{
   return trip.driverId == this.id;
 });
 }
@@ -21,15 +21,15 @@ let tripId = 0;
 passengers() {
    return this.trips().map(trip => {
       return trip.passenger();
-    });
+    })
   }
-}
+};
 
 
 class Passenger {
   constructor(name) {
     this.name = name;
-    this.id = ++passengerId;
+    this.id = ++passengerID;
 
     store.passengers.push(this);
   }
@@ -38,36 +38,35 @@ class Passenger {
     return store.trips.filter(trip => {
       return trip.passengerId == this.id;
     });
-  }
+  };
 
 
   drivers() {
     return this.trips().map(trip => {
       return trip.driver();
     });
-  }
-}
+  };
+};
 
 class Trip {
   constructor(driver, passenger) {
-    this.id = tripId++;
+    this.id = ++tripID;
     this.driverId = driver.id;
-    this.passengerId = passenger.id
+    this.passengerId = passenger.id;
 
     store.trips.push(this);
-  }
-
+  };
 
 
   driver() {
     return store.drivers.find(driver => {
       return driver.id === this.driverId;
-    });
-  }
+    })
+  };
 
  passenger() {
       return store.passengers.find(passenger => {
       return passenger.id === this.passengerId;
-    });
-  }
-}
+    })
+  };
+};
